944dd6b88339bf1df4ec18ec17f9e321f19947de
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-10-02 17:10:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Изучаю HTML</title>
+</head>
+<body>
+  <h1>Мой первый сайт</h1>
+  <h2>Я учусь веб-разработке</h2>
+
+  <p>Это абзац текста. Я могу писать здесь всё что угодно.</p>
+
+  <a href="https://google.com" target="_blank">Перейти в Google</a>
+
+  <br><br>
+
+  <img src="https://picsum.photos/300/200" alt="Картинка">
+  
+</body>
+</html>
