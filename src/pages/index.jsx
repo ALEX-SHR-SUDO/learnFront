6b60554f51de0053512cd5e03d625ff407abc7a1
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-10-26 14:33:44 +0200

    Добавлен файл index.jsx с кодом страницы Next.js

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import LogoUploader from '../components/LogoUploader';
+import '../styles/globals.css';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>LearnFront: Загрузка логотипа (Next.js)</h1>
+      <LogoUploader />
+    </main>
+  );
+}
