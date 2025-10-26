09b3330882260c56326cbcc6a52e8021d606a81c
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-10-26 16:18:46 +0200

    Add index.jsx for Home component

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import LogoUploader from '../components/LogoUploader';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>LearnFront: Загрузка логотипа (Next.js)</h1>
+      <LogoUploader />
+    </main>
+  );
+}
